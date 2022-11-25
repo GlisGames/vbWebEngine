e5589495085544f9270f0fdc0317157f3a4f50aa
Haozhong Zheng <zflphz@gmail.com>
2022-11-25 18:39:13 +0700

    shuffle & range

--- misc/vbUtils.ts
+++ misc/vbUtils.ts
@@ -4,10 +4,18 @@ declare global {
         clear(): void;
         front(): T;
         back(): T;
+        swap(index1: number, index2: number): void;
         /**
          * Remove the matched item once at a time
          */
         removeOnce(item: T): void;
+    }
+
+    interface ArrayConstructor {
+        /**
+         *  Generate a list of numbers
+         */
+        range(start: number, stop?: number, step?: number): number[];
     }
 
     interface String {
@@ -34,10 +42,14 @@ declare global {
     interface Math {
         randInt(max: number): number;
         /**
-         * @param [probs] array of probabilities that has the sum of 1
+         * @param [probs] Array of probabilities that has the sum of 1
          * @return index of the picked probability
          */
         randPickFromProbs(probs: number[]): number;
+        /**
+         * @param [n] How many times to perform random element swap, default is the length of array.
+         */
+        shuffle(arr: any[], n?: number): void;
     }
 }
 
@@ -51,11 +63,25 @@ Array.prototype.front = function() {
 Array.prototype.back = function() {
     return this[this.length - 1];
 }
+Array.prototype.swap = function(index1: number, index2: number) {
+    let tmp = this[index1];
+    this[index1] = this[index2];
+    this[index2] = tmp;
+}
 Array.prototype.removeOnce = function(item: any) {
     let index = this.indexOf(item);
     if (index > -1) {
         this.splice(index, 1);
     }
+}
+
+Array.range = function range(start: number, stop?: number, step?: number) {
+    if (step === undefined) step = 1;
+    if (stop === undefined) {
+        stop = start;
+        start = 0;
+    }
+    return Array.from({ length: (stop - start) / step }, (_, i) => start + (i * <any>step));
 }
 
 String.prototype.mapReplace = function(objects: { [from: string]: string | number }) {
@@ -95,6 +121,12 @@ Math.randPickFromProbs = function(probs: number[]) {
         if (r <= prob) return index;
     }
     return -1;
+}
+Math.shuffle = function(arr: any[], n?: number) {
+    if (n === undefined) n = arr.length;
+    for (let i = 0; i < n; i++) {
+        arr.swap(i, Math.randInt(arr.length));
+    }
 }
 
 
