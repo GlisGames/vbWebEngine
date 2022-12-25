cc2b7cc688b2c29b62f016e72a54c0469baf7435
Haozhong Zheng <zflphz@gmail.com>
2022-12-25 13:30:41 +0700

    update powershell script

--- dev-tools/setup-proj.ps1
+++ dev-tools/setup-proj.ps1
@@ -5,6 +5,8 @@ $CurrDir = $PWD
 $ProjDir = (Get-Item $PSScriptRoot).Parent.Parent.Parent.FullName
 
 function CheckPackages {
+    Write-Host "Check git" -ForegroundColor Green
+    git --version
     Write-Host "Check pnpm" -ForegroundColor Green
     npm list -g pnpm
     if ($LASTEXITCODE -ne 0) {
@@ -31,7 +33,7 @@ function BuildLibrary {
 
     Write-Host "Clone pixi-spine-tmp repo from github" -ForegroundColor Green
     git clone --depth=1 https://github.com/hmpthz/pixi-spine-tmp.git
-    if ($LASTEXITCODE -ne 0) { throw "Error" }
+    if ($LASTEXITCODE -ne 0) { throw "Error running `"git clone`"" }
     Set-Location "pixi-spine-tmp"
 
     Write-Host "Check @microsoft/rush" -ForegroundColor Green
@@ -43,20 +45,36 @@ function BuildLibrary {
 
     Write-Host "Run `"rush update`"" -ForegroundColor Green
     rush update
-    if ($LASTEXITCODE -ne 0) { throw "Error" }
+    if ($LASTEXITCODE -ne 0) { throw "Error running `"rush update`"" }
     Write-Host "Run `"npm install`"" -ForegroundColor Green
     npm i
-    if ($LASTEXITCODE -ne 0) { throw "Error" }
+    if ($LASTEXITCODE -ne 0) { throw "Error running `"npm install`"" }
     Write-Host "Run `"npm run build`"" -ForegroundColor Green
     npm run build
-    if ($LASTEXITCODE -ne 0) { throw "Error" }
+    if ($LASTEXITCODE -ne 0) { throw "Error running `"npm run build`"" }
 }
 
 function SetupProject {
     Set-Location $ProjDir
+
+    if ($Build) {
+        Write-Host "Check node_modules and pnpm-lock.yaml" -ForegroundColor Green
+        if (Test-Path "node_modules") {
+            Write-Host "Delete existing node_modules" -ForegroundColor Green
+            Remove-Item "node_modules" -Recurse -Force
+        }
+        if (Test-Path "pnpm-lock.yaml") {
+            Remove-Item "pnpm-lock.yaml" -Recurse -Force
+        }
+    }
+
     Write-Host "Run `"pnpm install`"" -ForegroundColor Green
     pnpm i
-    if ($LASTEXITCODE -ne 0) { throw "Error" }
+    if ($LASTEXITCODE -ne 0) { throw "Error running `"pnpm install`"" }
+
+    Write-Host "Run `"pnpm run type-check`"" -ForegroundColor Green
+    pnpm run type-check
+    if ($LASTEXITCODE -ne 0) { throw "Type check failed!" }
 }
 
 try {
@@ -65,8 +83,10 @@ try {
         BuildLibrary
     }
     SetupProject
+    Write-Host "Project setup succeeded!" -ForegroundColor Green
 }
 catch {
+    Write-Host $PSItem.Exception.Message -ForegroundColor Red
     Write-Host "Project setup failed!" -ForegroundColor Red
 }
 finally {
